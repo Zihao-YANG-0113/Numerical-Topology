9085c5fb5857b9ff0b3495c9dc2a900969cbfc37
Zihao-YANG-0113 <shil6682@ox.ac.uk>
2026-04-18 01:04:58 +0800

    cosheaf_core.py

--- tests/test_leray_example.py
+++
@@ -1,9 +0,0 @@
-from leray_c4_combinatorial import report_dict
-
-
-def test_leray_c4_barcode_and_checks():
-    rep = report_dict()
-    assert rep['monomorphism_check'] is True
-    assert rep['naturality_check'] is True
-    assert rep['barcode_H0'] == ['[0,2)', '[0,∞)'] or rep['barcode_H0'] == ['[0,∞)', '[0,2)']
-    assert rep['barcode_H1'] == ['[3,∞)']
